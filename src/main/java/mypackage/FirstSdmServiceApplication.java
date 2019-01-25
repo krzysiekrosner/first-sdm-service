ecb409e0526bcf696104a77ae8259a8284c79bee
Krzysztof Rosner <krosner@guidewire.com>
2019-01-25 10:10:52 +0100

    Initial commit from Atomist

---
+++ src/main/java/mypackage/FirstSdmServiceApplication.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSdmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSdmServiceApplication.class, args);
+	}
+}
